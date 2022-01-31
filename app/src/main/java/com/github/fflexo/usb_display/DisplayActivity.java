6e82a362f6fa3cae5e84e18197946509fce59d45
fflexo <1144937+fflexo@users.noreply.github.com>
2022-01-31 19:46:11 +0000

    bumped api level to 19 so we can do a better fullscreen

--- app/src/main/java/com/github/fflexo/usb_display/DisplayActivity.java
+++ app/src/main/java/com/github/fflexo/usb_display/DisplayActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 import android.widget.TextView;
 
 import com.github.fflexo.usb_display.databinding.ActivityMainBinding;
@@ -26,6 +27,12 @@ public class DisplayActivity extends /*AppCompatActivity*/ Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(flags);
 
         Display display = getWindowManager().getDefaultDisplay();
         Point displaySize = new Point();
